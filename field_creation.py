19e2ca8a58af9c9e8f9569632bc237e6e2164f4a
voranc223 <157906296+voranc223@users.noreply.github.com>
2024-03-02 17:44:44 +0100

    Update field_creation.py

--- field_creation.py
+++ field_creation.py
@@ -1,8 +1,11 @@
 import pygame
+import random
 from object_creation import Figure
+from Colors import colors
 
 
 pygame.init()
+
 
 class Tetris:
     def __init__(self, height, width):
@@ -13,10 +16,12 @@ class Tetris:
         self.height = height
         self.width = width
         self.figure = None
-        self.field=[]
+        self.field = []
         self.score = 0
         self.lines = 0
         self.level = 1
+        self.next_type_figure = random.randint(0, 6)
+        self.next_figure_color = random.randint(1, 6)
         self.clear_sound = pygame.mixer.Sound("Sounds/clear.ogg")
         self.rotate_sound = pygame.mixer.Sound("Sounds/rotate.ogg")
         pygame.mixer.music.load("Sounds/Tetris.theme.sound.mp3")
@@ -26,20 +31,33 @@ class Tetris:
             for y in range(self.width):
                 nova_linija.append(0)
             self.field.append(nova_linija)
-    def nova_figura(self):      #ustvari figuro in ji določi začetne koordinate
+
+    def nova_figura(self):      # ustvari figuro in ji določi začetne koordinate
         self.figure = Figure(3, 0)
+        self.figure.type = self.next_type_figure
+        self.figure.color = self.next_figure_color
+        self.next_type_figure = random.randint(0, len(self.figure.figures)-1)
+        self.next_figure_color = random.randint(1, len(colors)-1)
+
+    def image(self):
+        return self.figure.figures[self.figure.type][self.figure.rotation]
+
+    def image_next(self):
+        return self.figure.figures[self.next_type_figure][0]
+
     def intersekcija(self):
         intersection = False
         for i in range(4):
             for j in range(4):
                 a = i * 4 + j
-                if a in self.figure.image():
+                if a in self.image():
                     if i + self.figure.y > self.height - 1 or \
                             j + self.figure.x > self.width - 1 or\
                             j + self.figure.x < 0 or\
                             self.field[i + game.figure.y][j+game.figure.x] > 0:
-                                intersection = True
+                        intersection = True
         return intersection
+
     def uniči_polne_vrste(self):
         for i in range(1, self.height):
             a = 0
@@ -58,7 +76,7 @@ class Tetris:
         for i in range(4):
             for y in range(4):
                 a = i * 4 + y
-                if a in self.figure.image():
+                if a in self.image():
                     self.field[self.figure.y + i][self.figure.x + y] = self.figure.color
         self.nova_figura()
         self.uniči_polne_vrste()
@@ -70,19 +88,24 @@ class Tetris:
         if self.intersekcija():
             self.figure.y -= 1
             self.zamrzni()
-    def pojdi_v_stran(self,x):
+
+    def pojdi_v_stran(self, x):
         self.figure.x += x
         if self.intersekcija():
             self.figure.x -= x
+
     def rotiraj_se(self):
         last_rotation = self.figure.rotation
         self.figure.rotate()
         pygame.mixer.Sound.play(self.rotate_sound)
         if self.intersekcija():
             self.figure.rotation = last_rotation
+
     def pojdi_do_dna(self):
         while not self.intersekcija():
             self.figure.y += 1
         self.figure.y -= 1
         self.zamrzni()
+
+
 game = Tetris(20, 10)
